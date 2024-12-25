1043fa2af90f4d0138fe2562916025a88535d69a
Anton <ForWorkCodes@gmail.com>
2024-12-25 17:42:23 +0000

    base for api

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://genesisdata.site'),
 
     /*
     |--------------------------------------------------------------------------
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
 
 Route::post('/register', [UserController::class, 'register']);
-Route::post('/progress', [UserController::class, 'updateProgress']);
+
+//Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/progress', [UserController::class, 'updateProgress']);
+    Route::get('/user', [UserController::class, 'show']);
+//});
